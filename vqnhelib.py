d83c583381f06eec15d1ad4f0b134db0ce54e195
James <125136785+miinukim@users.noreply.github.com>
2025-08-22 16:10:10 +0900

    code updates

--- vqnhelib.py
+++ vqnhelib.py
@@ -241,7 +241,7 @@ class HeiAnsatzCircuit(QuantumCircuit):
         return qc, star_qubit
 
 
-# Trainer class for the vanilla VQE
+
 class VQETrainer:
     def __init__(self, ansatz, hamiltonian, shots=None, noise_model=None, print_mode = False):
         self.ansatz = ansatz
@@ -335,7 +335,6 @@ class VQETrainer:
         return result
 
 
-# Neural network definitions
 class NeuralNet(nn.Module):
     def __init__(self, num_qubits, features):
         super(NeuralNet, self).__init__()
@@ -586,7 +585,7 @@ class VQNHETrainer:
         else: return self.nnet, loss_list
 
 
-## U-VQNHE Trainer
+## VQNHE Trainer with unitary neural network
 class UnitaryVQNHETrainer:
     def __init__(self, ansatz_circuit: Union[IsingAnsatzCircuit, HeiAnsatzCircuit],
             params : ParameterVector, operator_list, coeff_list, nn_options: dict, 
@@ -773,3 +772,366 @@ class UnitaryVQNHETrainer:
         if self.print_mode: print('Finished Training')
         
         return self.nnet, loss_list
+
+
+## VQNHE Trainer
+class ModifiedVQNHETrainer:
+    def __init__(self, ansatz_circuit: Union[IsingAnsatzCircuit, HeiAnsatzCircuit],
+            params : ParameterVector, operator_list, coeff_list, nn_options: dict, 
+            qc_options: Optional[dict] = {}, print_mode = False):
+        '''
+        qc_options (optional, dict): ['shots', 'noise_model']
+        nn_options (dict): ['num_epoch', 'features', 'lr'(optional)]
+        '''
+        self.ansatz_circuit = ansatz_circuit
+        self.num_qubits = ansatz_circuit.num_qubits
+        self.shots = qc_options.get('shots', 2**self.num_qubits)
+        self.noise_model = qc_options.get('noise_model', None)
+        self.sampler = Sampler(backend_options = {'shots': self.shots, 'noise_model': self.noise_model})
+        self.operator_list = operator_list
+        self.coeff_list = coeff_list
+        self.params = params
+        self.print_mode = print_mode
+
+        if 'num_epoch' not in nn_options.keys() or 'features' not in nn_options.keys(): 
+            raise ValueError("nn_options must include 'num_epoch' and 'features'") 
+        self.num_epoch = nn_options['num_epoch']
+        self.features = nn_options['features']
+        self.lr = nn_options.get('lr', 1e-3)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if self.print_mode: print(f"Torch running on " + self.device)
+
+        self.nnet = NeuralNet(num_qubits = self.num_qubits, features = self.features).to(self.device)
+        betas = (0.9, 0.999)
+        self.optimizer = torch.optim.Adam(self.nnet.parameters(), lr = self.lr, betas = betas)
+
+
+    def _pauli_transformation(self, bstr: Union[torch.Tensor, list], pauli_str: str) -> torch.Tensor:
+        # Ensure bstr is a tensor of integers
+        if type(bstr) == list: bstr = torch.tensor(bstr)
+        conj_list = torch.ones_like(bstr, dtype=torch.float32)
+
+        for i in range(len(pauli_str)):
+            if pauli_str[i] in ["X", "Y"]:
+                conj_list[i] = -1 if bstr[i] == 1 else 1
+            else:
+                conj_list[i] = bstr[i]
+
+        return conj_list
+
+
+    def _run_sampler(self, qc):
+        result = self.sampler.run(qc, parameter_values=self.params).result().quasi_dists[0].binary_probabilities()
+        return result
+
+    def train(self, show_process = True):
+        self.nnet.train()
+        loss_list = []
+        total_loss = 0.0
+        self.optimizer.zero_grad()
+        
+        # Run the ansatz circuit
+        qc_ans, _ = self.ansatz_circuit.generate_measurement_circuit("I" * self.num_qubits)
+        param_dict = {param: value for param, value in zip(qc_ans.parameters, self.params)}
+        qc_ans.assign_parameters(param_dict)
+        ansatz_res = self._run_sampler(qc_ans)
+        ansatz_keys = {s[::-1] for s in ansatz_res.keys()}
+
+        # Run the measurement circuits
+        meas_res = {}
+        star_idxs = {}
+
+        meas_keys = set()
+        meas_trans_keys = set()
+
+        for pstr in self.operator_list:
+            qc_meas, star_idx = self.ansatz_circuit.generate_measurement_circuit(pstr)
+            qc_meas.assign_parameters(self.params)
+            pstr_res = self._run_sampler(qc_meas)
+            meas_res[pstr] = pstr_res
+            star_idxs[pstr] = star_idx
+            meas_key = {s[::-1] for s in pstr_res.keys()}
+            meas_keys = meas_keys.union(meas_key)
+
+            for iter_str in pstr_res.keys():
+                s_str = [(2 * int(x) - 1) for x in iter_str]
+                s_str.reverse()
+                if star_idx >= 0: # Pauli string has 'X' or 'Y'.
+                    if s_str[star_idx] == 1:
+                        s_str[star_idx] = -1
+                s_conj = self._pauli_transformation(s_str, pstr)
+                s_conj_str = ''.join(['1' if x == 1 else '0' for x in s_conj.cpu().detach().numpy()])
+                s_string = ''.join(['1' if x == 1 else '0' for x in s_str])
+                meas_trans_keys.add(s_conj_str)
+                meas_keys.add(s_string)
+
+        total_meas_keys = meas_keys.union(meas_trans_keys)
+
+        # add 1e-7 to the bit strings that are not in the ansatz result
+        eps = 1e-7
+
+        pre_ansatz = ansatz_res
+
+        ansatz_unique_items = list(ansatz_keys.difference(total_meas_keys))
+        ansatz_keys = ansatz_keys.intersection(total_meas_keys)
+        for bstr in list(ansatz_res.keys()):
+            ansatz_res[bstr] = (ansatz_res[bstr] - eps if ansatz_res[bstr] > 0 else ansatz_res + eps)
+            if bstr[::-1] in ansatz_unique_items:
+                del ansatz_res[bstr]
+        
+        meas_unique_items = list(total_meas_keys.difference(ansatz_keys))     # (bstr_t U bstr_m) \ bstr_a
+        meas_unique_count = len(meas_unique_items)
+        ansatz_count = len(ansatz_keys)
+        scaled_eps = eps * ansatz_count / np.max([meas_unique_count, 1])
+
+        f = 0
+
+        for bstr in meas_unique_items:
+            ansatz_res[bstr[::-1]] = scaled_eps
+
+        for pstr in self.operator_list:
+            pstr_res = meas_res[pstr]
+            pstr_keys = {s[::-1] for s in pstr_res.keys()}
+            meas_not_in_pstr = list(total_meas_keys.difference(pstr_keys))
+            for bstr in pstr_keys:
+                pstr_res[bstr[::-1]] = pstr_res[bstr[::-1]] - eps
+            
+            for bstr in meas_not_in_pstr:
+                pstr_res[bstr[::-1]] = eps * len(pstr_keys) / np.max([len(meas_not_in_pstr), 1])
+            
+            meas_res[pstr] = pstr_res
+
+        for s, _ in ansatz_res.items():
+            p = ansatz_res[s]
+            pp = pre_ansatz.get(s, 0)
+            f += np.sqrt(p) * np.sqrt(pp)
+                
+        if self.print_mode: print(f"State Fidelity: {(f*f):.6f}")
+
+        # Neural network training
+        iterator = tqdm(range(self.num_epoch), desc="Processing") if show_process else range(self.num_epoch)
+        for epoch in iterator:
+            ham = 0.0
+            denominator = 0.0
+
+            for a_str, a_prob in ansatz_res.items():
+                a_list = torch.tensor([int(x) for x in a_str], dtype=torch.float32, device=self.device)
+                a_tensor = 2 * a_list - 1
+                a_tensor = a_tensor.flip(dims=[0])
+                fs_denom = self.nnet(a_tensor)
+                denominator += fs_denom * fs_denom * a_prob
+
+            for idx in range(len(self.operator_list)):
+                pstr = self.operator_list[idx]
+                string_prob = meas_res[pstr]
+                numerator = 0.0
+
+                for m_str, m_prob in string_prob.items():
+                    sign = 1
+                    m_list = torch.tensor([int(x) for x in m_str], dtype=torch.float32, device=self.device)
+                    m_tensor = 2 * m_list - 1
+                    m_tensor = m_tensor.flip(dims = [0])
+                    if star_idxs[pstr] >= 0:
+                        if m_tensor[star_idxs[pstr]] == 1:
+                            sign *= -1
+                            m_tensor[star_idxs[pstr]] *= -1
+                        for i in range(len(pstr)):
+                            if pstr[i] == 'Z' and m_tensor[i] == 1:
+                                sign *= -1
+                        m_transformed = self._pauli_transformation(m_tensor, pstr)
+                        fs = self.nnet(m_tensor)
+                        fs_t = self.nnet(m_transformed)
+                        numerator += fs * fs_t * m_prob * sign
+                    else:
+                        fs = self.nnet(m_tensor)
+                        for i in range(len(pstr)):
+                            if pstr[i] == 'Z' and m_tensor[i] == 1:
+                                sign *= -1
+                        numerator += (fs ** 2) * m_prob * sign
+
+                mean_pauli = numerator / denominator * self.coeff_list[idx]
+                ham += mean_pauli
+            
+            running_loss = ham
+            running_loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            total_loss += ham.item()
+
+            loss_list.append(ham.item())
+        
+        if self.print_mode: print('Finished Training')
+        
+        return self.nnet, loss_list
+    
+
+# VQNHE Trainer with exact results (using statevector simulator)
+class ExactVQNHETrainer:
+    def __init__(self, ansatz_circuit: Union[IsingAnsatzCircuit, HeiAnsatzCircuit],
+            params : ParameterVector, operator_list, coeff_list, nn_options: dict, 
+            print_mode = False):
+        '''
+        qc_options (optional, dict): ['shots', 'noise_model']
+        nn_options (dict): ['num_epoch', 'features', 'lr'(optional)]
+        '''
+        self.ansatz_circuit = ansatz_circuit
+        self.num_qubits = ansatz_circuit.num_qubits
+        self.operator_list = operator_list
+        self.coeff_list = coeff_list
+        self.params = params
+        self.print_mode = print_mode
+
+        if 'num_epoch' not in nn_options.keys() or 'features' not in nn_options.keys(): 
+            raise ValueError("nn_options must include 'num_epoch' and 'features'") 
+        self.num_epoch = nn_options['num_epoch']
+        self.features = nn_options['features']
+        self.lr = nn_options.get('lr', 1e-3)
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        if self.print_mode: print(f"Torch running on " + self.device)
+
+        self.nnet = NeuralNet(num_qubits = self.num_qubits, features = self.features).to(self.device)
+        betas = (0.9, 0.999)
+        self.optimizer = torch.optim.Adam(self.nnet.parameters(), lr = self.lr, betas = betas)
+
+
+    def _pauli_transformation(self, bstr: torch.Tensor, pauli_str: str) -> torch.Tensor:
+        # Ensure bstr is a tensor of integers
+        conj_list = torch.ones_like(bstr, dtype=torch.float32)
+
+        for i in range(len(pauli_str)):
+            if pauli_str[i] in ["X", "Y"]:
+                conj_list[i] = -1 if bstr[i] == 1 else 1
+            else:
+                conj_list[i] = bstr[i]
+
+        return conj_list
+
+
+    def _run_qc(self, qc):
+        statevector = Statevector(qc)
+        probabilities = statevector.probabilities()
+        result = {format(i, f'0{self.num_qubits}b'): prob for i, prob in enumerate(probabilities)}
+        return result
+
+    def train(self, show_process = True):
+        self.nnet.train()
+        loss_list = []
+        total_loss = 0.0
+        self.optimizer.zero_grad()
+        
+        # Run the ansatz circuit
+        qc_ans, _ = self.ansatz_circuit.generate_measurement_circuit("I" * self.num_qubits, measure = False)
+        param_dict = {param: value for param, value in zip(qc_ans.parameters, self.params)}
+        qc_ans.assign_parameters(param_dict, inplace=True)
+        ansatz_res = self._run_qc(qc_ans)
+
+        # Run the measurement circuits
+        meas_res = {}
+        star_idxs = {}
+
+        for pstr in self.operator_list:
+            qc_meas, star_idx = self.ansatz_circuit.generate_measurement_circuit(pstr, measure = False)
+            qc_meas.assign_parameters(self.params, inplace=True)
+            meas_res[pstr] = self._run_qc(qc_meas)
+            star_idxs[pstr] = star_idx
+
+        # Neural network training
+        iterator = tqdm(range(self.num_epoch), desc="Processing") if show_process else range(self.num_epoch)
+        for epoch in iterator:
+            ham = 0.0
+            denominator = 0.0
+
+            for a_str, a_prob in ansatz_res.items():
+                a_list = torch.tensor([int(x) for x in a_str], dtype=torch.float32, device=self.device)
+                a_tensor = 2 * a_list - 1
+                a_tensor = a_tensor.flip(dims=[0])
+                fs_denom = self.nnet(a_tensor)
+                denominator += fs_denom * fs_denom * a_prob
+
+            for idx in range(len(self.operator_list)):
+                pstr = self.operator_list[idx]
+                string_prob = meas_res[pstr]
+                numerator = 0.0
+
+                for m_str, m_prob in string_prob.items():
+                    sign = 1
+                    m_list = torch.tensor([int(x) for x in m_str], dtype=torch.float32, device=self.device)
+                    m_tensor = 2 * m_list - 1
+                    m_tensor = m_tensor.flip(dims = [0])
+                    if star_idxs[pstr] >= 0:
+                        if m_tensor[star_idxs[pstr]] == 1:
+                            sign *= -1
+                            m_tensor[star_idxs[pstr]] *= -1
+                        for i in range(len(pstr)):
+                            if pstr[i] == 'Z' and m_tensor[i] == 1:
+                                sign *= -1
+                        m_transformed = self._pauli_transformation(m_tensor, pstr)
+                        fs = self.nnet(m_tensor)
+                        fs_t = self.nnet(m_transformed)
+                        numerator += fs * fs_t * m_prob * sign
+                    else:
+                        fs = self.nnet(m_tensor)
+                        for i in range(len(pstr)):
+                            if pstr[i] == 'Z' and m_tensor[i] == 1:
+                                sign *= -1
+                        numerator += (fs ** 2) * m_prob * sign
+
+                mean_pauli = numerator / denominator * self.coeff_list[idx]
+                ham += mean_pauli
+            
+            running_loss = ham
+            running_loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            total_loss += ham.item()
+
+            loss_list.append(ham.item())
+        
+        if self.print_mode: print('Finished Training')
+        
+        return self.nnet, loss_list, ansatz_res, meas_res
+
+
+
+# Plot the plots!
+class VQNHEPlotter:
+    def __init__(self, num_qubits, nnet, loss_list, exp_type, vqe = None, exact = None):
+        self.num_qubits = num_qubits
+        self.nnet = nnet
+        self.loss_list = loss_list
+        self.exp_type = exp_type
+        self.vqe = vqe
+        self.exact = exact
+        self._loss_plot()
+        if self.nnet is not None: self._bitstr_plot()
+
+    def _loss_plot(self):
+        plot_name = f"{self.num_qubits}q_{self.exp_type}_loss.jpg"
+        plt.plot(range(len(self.loss_list)), self.loss_list, label = 'VQNHE')
+        if self.vqe is not None: plt.plot(range(len(self.loss_list)), [self.vqe for _ in range(len(self.loss_list))], label = 'VQE')
+        if self.exact is not None: plt.plot(range(len(self.loss_list)), [self.exact  for _ in range(len(self.loss_list))], label = 'Exact')
+
+        plt.xlabel('epoch (5)')
+        plt.ylabel('Expectation value')
+        plt.title(f'VQNHE on {self.num_qubits} qubits')
+        plt.legend()
+        plt.savefig('./result/' + plot_name, dpi = 300)
+
+    
+    def _bitstr_plot(self):
+        plot_name = f"{self.num_qubits}q_{self.exp_type}_bitstr.jpg"
+        
+        nn_res = []
+        for s in range(2**self.num_qubits):
+            ss = [int(digit) for digit in bin(s)[2:].zfill(self.num_qubits)]
+            ss.reverse()
+            ss = torch.tensor(ss, device = next(self.nnet.parameters()).device, dtype=torch.float32)
+            nn_res.append(self.nnet(ss).cpu().detach().numpy())
+
+        plt.clf()
+        plt.plot(nn_res, label = 'Neural Network')
+        plt.xlabel('s')
+        plt.ylabel('f(s)')
+        plt.title(f"{self.num_qubits}-qubit VQNHE: Neural Network Landscape")
+        plt.legend()
+        plt.savefig('./result/' + plot_name, dpi = 300)
